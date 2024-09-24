945dc99638ebabd3909827a8b6bd1964d80dc80d
beknursailaubek <b3knursailaub3k@gmail.com>
2024-09-24 11:52:39 +0500

    Pagination

--- idiamarket-server/controllers/categoryController.js
+++ idiamarket-server/controllers/categoryController.js
@@ -4,201 +4,222 @@ const Product = require("../models/product");
 
 // Create a new Category
 async function buildUri(parentId, slugifiedTitle) {
-    if (!parentId) {
-        return slugifiedTitle;
-    }
+  if (!parentId) {
+    return slugifiedTitle;
+  }
 
-    const parentCategory = await Category.findById(parentId);
+  const parentCategory = await Category.findById(parentId);
 
-    if (!parentCategory) {
-        throw new Error("Parent category not found");
-    }
+  if (!parentCategory) {
+    throw new Error("Parent category not found");
+  }
 
-    const parentUri = await buildUri(parentCategory.parent, parentCategory.category_code);
+  const parentUri = await buildUri(parentCategory.parent, parentCategory.category_code);
 
-    return `${parentUri}/${slugifiedTitle}`;
+  return `${parentUri}/${slugifiedTitle}`;
 }
 
 exports.createCategory = async (req, res) => {
-    try {
-        const slugifiedTitle = slugify(req.body.title, { lower: true, strict: true });
-        let categoryUri = slugifiedTitle;
+  try {
+    const slugifiedTitle = slugify(req.body.title, { lower: true, strict: true });
+    let categoryUri = slugifiedTitle;
 
-        if (req.body.parent) {
-            categoryUri = await buildUri(req.body.parent, slugifiedTitle);
-        }
-
-        const category = new Category({
-            ...req.body,
-            category_code: slugifiedTitle,
-            uri: categoryUri,
-        });
-
-        await category.save();
-
-        if (req.body.parent) {
-            await Category.findByIdAndUpdate(req.body.parent, {
-                $push: { children: category._id },
-            });
-        }
-
-        res.status(201).send(category);
-    } catch (error) {
-        res.status(400).send({ error: error.message });
+    if (req.body.parent) {
+      categoryUri = await buildUri(req.body.parent, slugifiedTitle);
     }
+
+    const category = new Category({
+      ...req.body,
+      category_code: slugifiedTitle,
+      uri: categoryUri,
+    });
+
+    await category.save();
+
+    if (req.body.parent) {
+      await Category.findByIdAndUpdate(req.body.parent, {
+        $push: { children: category._id },
+      });
+    }
+
+    res.status(201).send(category);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 };
 
 // Get all Categories
 exports.getAllCategories = async (req, res) => {
-    try {
-        const categories = await Category.find({});
-        res.send(categories);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  try {
+    const categories = await Category.find({});
+    res.send(categories);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 // Get a single Category by ID
 exports.getCategoryById = async (req, res) => {
-    try {
-        const category = await Category.findById(req.params.id);
+  try {
+    const category = await Category.findById(req.params.id);
 
-        if (!category) {
-            return res.status(404).send({ error: "Category not found" });
-        }
-
-        res.send(category);
-    } catch (error) {
-        res.status(500).send(error);
+    if (!category) {
+      return res.status(404).send({ error: "Category not found" });
     }
+
+    res.send(category);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 // Update a Category by ID
 exports.updateCategory = async (req, res) => {
-    try {
-        const updates = req.body;
+  try {
+    const updates = req.body;
 
-        // If title is being updated, regenerate category_code
-        if (updates.title) {
-            updates.category_code = slugify(updates.title, { lower: true, strict: true });
-        }
-
-        const category = await Category.findByIdAndUpdate(req.params.id, updates, { new: true, runValidators: true });
-
-        if (!category) {
-            return res.status(404).send({ error: "Category not found" });
-        }
-
-        res.send(category);
-    } catch (error) {
-        res.status(400).send(error);
+    // If title is being updated, regenerate category_code
+    if (updates.title) {
+      updates.category_code = slugify(updates.title, { lower: true, strict: true });
     }
+
+    const category = await Category.findByIdAndUpdate(req.params.id, updates, { new: true, runValidators: true });
+
+    if (!category) {
+      return res.status(404).send({ error: "Category not found" });
+    }
+
+    res.send(category);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
 // Delete a Category by ID
 exports.deleteCategory = async (req, res) => {
-    try {
-        const category = await Category.findByIdAndDelete(req.params.id);
+  try {
+    const category = await Category.findByIdAndDelete(req.params.id);
 
-        if (!category) {
-            return res.status(404).send({ error: "Category not found" });
-        }
-
-        if (category.parent) {
-            await Category.findByIdAndUpdate(category.parent, {
-                $pull: { children: category._id },
-            });
-        }
-
-        res.send({ message: "Category deleted successfully" });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
+    if (!category) {
+      return res.status(404).send({ error: "Category not found" });
     }
+
+    if (category.parent) {
+      await Category.findByIdAndUpdate(category.parent, {
+        $pull: { children: category._id },
+      });
+    }
+
+    res.send({ message: "Category deleted successfully" });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 };
 
 // Get Products by Category Code
 exports.getCategoryAndProductsByCategoryCode = async (req, res) => {
-    try {
-        const categoryCode = req.params.category_code;
+  try {
+    const categoryCode = req.params.category_code;
 
-        const category = await Category.findOne({ category_code: { $in: categoryCode } }).populate({
-            path: "children",
-            model: "Category",
-        });
+    const page = parseInt(req.query.page, 10) || 1;
 
-        if (!category) {
-            return res.status(404).send({ error: "Category not found" });
-        }
+    const limit = parseInt(req.query.limit, 10) || 20;
 
-        const products = await Product.find({ categories: category._id })
-            .populate("color")
-            .populate("categories")
-            .populate("short_description")
-            .populate({
-                path: "attributes",
-                populate: {
-                    path: "items",
-                    model: "AttributeItem",
-                },
-            })
-            .populate("variants.attributes")
-            .populate("stickers")
-            .populate("meta_data");
+    const skip = (page - 1) * limit;
 
-        res.send({ category, products });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
+    const category = await Category.findOne({ category_code: categoryCode }).populate({
+      path: "children",
+      model: "Category",
+    });
+
+    if (!category) {
+      return res.status(404).send({ error: "Category not found" });
     }
+
+    const products = await Product.find({ categories: category._id })
+      .populate("color")
+      .populate("categories")
+      .populate("short_description")
+      .populate({
+        path: "attributes",
+        populate: {
+          path: "items",
+          model: "AttributeItem",
+        },
+      })
+      .populate("variants.attributes")
+      .populate("stickers")
+      .populate("meta_data")
+      .skip(skip)
+      .limit(limit);
+
+    const totalProducts = await Product.countDocuments({ categories: category._id });
+
+    const totalPages = Math.ceil(totalProducts / limit);
+
+    res.send({
+      category,
+      products,
+      pagination: {
+        totalProducts,
+        totalPages,
+        currentPage: page,
+        pageSize: limit,
+      },
+    });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 };
 
 // Count Products by Category Code
 exports.countProductsByCategoryCodes = async (req, res) => {
-    try {
-        const categoryCodes = req.body.category_codes;
-        const categories = await Category.find({ category_code: { $in: categoryCodes } });
-        const counts = {};
+  try {
+    const categoryCodes = req.body.category_codes;
+    const categories = await Category.find({ category_code: { $in: categoryCodes } });
+    const counts = {};
 
-        for (const category of categories) {
-            const productCount = await Product.countDocuments({ categories: category._id });
-            counts[category.category_code] = productCount;
-        }
-
-        res.send(counts);
-    } catch (error) {
-        res.status(500).send({ error: error.message });
+    for (const category of categories) {
+      const productCount = await Product.countDocuments({ categories: category._id });
+      counts[category.category_code] = productCount;
     }
+
+    res.send(counts);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 };
 
 // Controller function to get breadcrumbs by category_code
 exports.getBreadcrumbs = async (req, res) => {
-    const { category_code } = req.params;
+  const { category_code } = req.params;
 
-    try {
-        const breadcrumbs = await buildBreadcrumbs(category_code);
-        res.json(breadcrumbs);
-    } catch (error) {
-        console.error("Error fetching breadcrumbs:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
+  try {
+    const breadcrumbs = await buildBreadcrumbs(category_code);
+    res.json(breadcrumbs);
+  } catch (error) {
+    console.error("Error fetching breadcrumbs:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 const buildBreadcrumbs = async (category_code) => {
-    let breadcrumbs = [];
+  let breadcrumbs = [];
 
-    let category = await Category.findOne({ category_code }).populate("parent");
+  let category = await Category.findOne({ category_code }).populate("parent");
 
-    while (category) {
-        breadcrumbs.unshift({
-            name: category.title,
-            path: `/${category.source}/${category.uri}`,
-        });
+  while (category) {
+    breadcrumbs.unshift({
+      name: category.title,
+      path: `/${category.source}/${category.uri}`,
+    });
 
-        if (category.parent) {
-            category = await Category.findById(category.parent._id).populate("parent");
-        } else {
-            category = null;
-        }
+    if (category.parent) {
+      category = await Category.findById(category.parent._id).populate("parent");
+    } else {
+      category = null;
     }
+  }
 
-    return breadcrumbs;
+  return breadcrumbs;
 };
--- idiamarket/src/components/Sort/Sort.jsx
+++ idiamarket/src/components/Sort/Sort.jsx
@@ -4,59 +4,59 @@ import ArrowDown from "../../assets/images/icons/arrow-down.svg";
 import "./Sort.css";
 
 const Sort = ({ onSortChange }) => {
-    const [selectedOption, setSelectedOption] = useState("По популярности");
-    const [isOpen, setIsOpen] = useState(false);
-    const sortRef = useRef();
+  const [selectedOption, setSelectedOption] = useState("По популярности");
+  const [isOpen, setIsOpen] = useState(false);
+  const sortRef = useRef();
 
-    useEffect(() => {
-        // Add event listener when the dropdown is open
-        if (isOpen) {
-            document.addEventListener("mousedown", handleClickOutside);
-        } else {
-            document.removeEventListener("mousedown", handleClickOutside);
-        }
+  useEffect(() => {
+    // Add event listener when the dropdown is open
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
 
-        // Clean up the event listener on component unmount
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [isOpen]);
-
-    const handleClickOutside = (event) => {
-        if (sortRef.current && !sortRef.current.contains(event.target)) {
-            setIsOpen(false);
-        }
+    // Clean up the event listener on component unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
     };
+  }, [isOpen]);
 
-    const options = ["По популярности", "По скидке", "По новизне", "По возрастанию цены", "По убыванию цены"];
+  const handleClickOutside = (event) => {
+    if (sortRef.current && !sortRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
+  const options = ["По популярности", "По скидке", "По новизне", "По возрастанию цены", "По убыванию цены"];
 
-    const handleOptionClick = (option) => {
-        setSelectedOption(option);
-        setIsOpen(false);
-        onSortChange(option); // Trigger the sorting change
-    };
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div className="sort" ref={sortRef}>
-            <div className="sort__wrapper" onClick={toggleDropdown}>
-                {selectedOption}
-                <img className="sort__icon_arrow" src={isOpen ? ArrowUp : ArrowDown} alt="" />
-            </div>
-            {isOpen && (
-                <ul className="sort__options">
-                    {options.map((option) => (
-                        <li key={option} onClick={() => handleOptionClick(option)} className={`sort__option ${option === selectedOption ? "sort__option_selected" : ""}`}>
-                            {option}
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+    onSortChange(option); // Trigger the sorting change
+  };
+
+  return (
+    <div className="sort" ref={sortRef}>
+      <div className="sort__wrapper" onClick={toggleDropdown}>
+        {selectedOption}
+        <img className="sort__icon_arrow" src={isOpen ? ArrowUp : ArrowDown} alt="" />
+      </div>
+      {isOpen && (
+        <ul className="sort__options">
+          {options.map((option) => (
+            <li key={option} onClick={() => handleOptionClick(option)} className={`sort__option ${option === selectedOption ? "sort__option_selected" : ""}`}>
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Sort;
